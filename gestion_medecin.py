364cb68a0ac14f8c750269e469a429dd15dcaadd
hugo <hugo-mehdi.taj@epitech.eu>
2024-01-03 21:05:23 +0100

    [add] medecin with modif on interface, new menu

---
+++ gestion_medecin.py
@@ -0,0 +1,86 @@
+import sqlite3
+from datetime import datetime, timedelta
+
+class GestionMedecin:
+    def __init__(self, fichier_db='medecins.db'):
+        self.fichier_db = fichier_db
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.fichier_db)
+        self.c = self.conn.cursor()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.close()
+
+    def creer_tables(self):
+        if not hasattr(self, 'conn') or not self.conn:
+            self.conn = sqlite3.connect(self.fichier_db)
+            self.c = self.conn.cursor()
+
+        with self.conn:
+            self.c.execute('''
+                CREATE TABLE IF NOT EXISTS Medecin (
+                    ID_medecin INTEGER PRIMARY KEY,
+                    Nom TEXT,
+                    Prenom TEXT
+                )
+            ''')
+
+    def ajouter_medecin(self, prenom, nom):
+        with self.conn:
+            self.c.execute('''
+                INSERT INTO Medecin (Nom, Prenom)
+                VALUES (?, ?)
+            ''', (nom, prenom))
+
+            print("Médecin ajouté avec succès.")
+
+    def lister_medecins(self):
+        self.c.execute('''
+            SELECT ID_medecin, Nom, Prenom
+            FROM Medecin
+            ORDER BY Nom, Prenom
+        ''')
+        medecins = self.c.fetchall()
+
+        if not medecins:
+            print("Aucun médecin enregistré.")
+        else:
+            for medecin in medecins:
+                id_medecin, nom, prenom = medecin
+                print(f"ID: {id_medecin}, Nom: {nom}, Prénom: {prenom}")
+
+    def supprimer_medecin(self, id_medecin):
+        with self.conn:
+            self.c.execute('''
+                DELETE FROM Medecin
+                WHERE ID_medecin = ?
+            ''', (id_medecin,))
+
+            print("Médecin supprimé avec succès.")
+
+    def afficher_planning_semaine(self, id_medecin):
+        # Récupérer le nom et prénom du médecin
+        self.c.execute('''
+            SELECT Nom, Prenom
+            FROM Medecin
+            WHERE ID_medecin = ?
+        ''', (id_medecin,))
+        result = self.c.fetchone()
+
+        if result:
+            nom_medecin, prenom_medecin = result
+            print(f"Planning de la semaine pour le Dr {prenom_medecin} {nom_medecin} (ID: {id_medecin})\n")
+
+            # Générer les plages horaires de 8h à 20h
+            debut_journee = datetime.strptime('08:00', '%H:%M')
+            fin_journee = datetime.strptime('20:00', '%H:%M')
+            temps_entre_rdv = timedelta(minutes=30)
+            plage_horaire = debut_journee
+
+            while plage_horaire <= fin_journee:
+                print(f"{plage_horaire.strftime('%H:%M')} - {plage_horaire + temps_entre_rdv.strftime('%H:%M')}")
+                plage_horaire += temps_entre_rdv
+        else:
+            print(f"Aucun médecin trouvé avec l'ID {id_medecin}.")
--- interface.py
+++ interface.py
@@ -1,13 +1,17 @@
-import os, sys
+import os
+import sys
+from gestion_medecin import GestionMedecin
 from gestion_rdv import GestionRendezVous
 
 class InterfaceUtilisateur:
     def __init__(self):
         self.gestion_rdv = GestionRendezVous(fichier_db='rendezvous.db')
+        self.gestion_medecin = GestionMedecin(fichier_db='medecins.db')
 
     def menu_principal(self):
         try:
             self.gestion_rdv.creer_tables()
+            self.gestion_medecin.creer_tables()
         except FileNotFoundError as fnfe:
             print("\033[91mErreur : Fichier introuvable - {}\033[0m".format(str(fnfe)))
             sys.exit(1)
@@ -19,29 +23,88 @@ class InterfaceUtilisateur:
             os.system('clear')  # Utilisez 'cls' au lieu de 'clear' si vous êtes sur Windows
 
             print("\nMenu Principal:")
-            print("1. Afficher la liste de RDV")
-            print("2. Ajouter un RDV")
-            print("3. Modifier un RDV")
-            print("4. Supprimer un RDV")
-            print("5. Quitter")
+            print("1. Gestion des RDV")
+            print("2. Gestion des Médecins")
+            print("3. Quitter")
 
-            choix = input("Choisissez une option (1-5): ")
+            choix = input("Choisissez une option (1-3): ")
 
             if choix == '1':
-                date_jour = input("Entrez la date pour afficher les RDV (format YYYY-MM-DD): ")
-                self.gestion_rdv.lister_rendezvous(date_jour)
-                # self.gestion_rdv.lister_rendezvous()
+                self.menu_gestion_rdv()
             elif choix == '2':
-                self.menu_ajouter_rdv()
+                self.menu_gestion_medecins()
             elif choix == '3':
-                self.menu_modifier_rdv()
-            elif choix == '4':
-                self.menu_supprimer_rdv()
-            elif choix == '5':
                 print("Au revoir!")
                 break
             else:
                 print("Option invalide. Veuillez choisir une option valide.")
+
+    def menu_gestion_rdv(self):
+        while True:
+            os.system('clear')  # Retiré pour ne pas effacer le terminal pendant l'affichage des réponses
+
+            print("\nGestion des RDV:")
+            print("1. Afficher la liste de RDV")
+            print("2. Ajouter un RDV")
+            print("3. Modifier un RDV")
+            print("4. Supprimer un RDV")
+            print("5. Retour au menu principal")
+
+            choix_rdv = input("Choisissez une option (1-5): ")
+
+            if choix_rdv == '1':
+                self.gestion_rdv.lister_rendezvous()
+            elif choix_rdv == '2':
+                self.menu_ajouter_rdv()
+            elif choix_rdv == '3':
+                self.menu_modifier_rdv()
+            elif choix_rdv == '4':
+                self.menu_supprimer_rdv()
+            elif choix_rdv == '5':
+                break
+            else:
+                print("Option invalide. Veuillez choisir une option valide.")
+
+    def menu_gestion_medecins(self):
+        while True:
+            os.system('clear')  # Retiré pour ne pas effacer le terminal pendant l'affichage des réponses
+
+            print("\nGestion de Médecins:")
+            print("1. Ajouter un médecin")
+            print("2. Supprimer un médecin")
+            print("3. Lister les médecins")
+            print("4. Afficher le planning d'un médecin")
+            print("5. Retour au menu principal")
+
+            choix_medecin = input("Choisissez une option (1-5): ")
+
+            if choix_medecin == '1':
+                self.menu_ajouter_medecin()
+            elif choix_medecin == '2':
+                self.menu_supprimer_medecin()
+            elif choix_medecin == '3':
+                self.gestion_medecin.lister_medecins()
+            elif choix_medecin == '4':
+                self.menu_afficher_planning()
+            elif choix_medecin == '5':
+                break
+            else:
+                print("Option invalide. Veuillez choisir une option valide.")
+
+    def menu_ajouter_medecin(self):
+        prenom_medecin = input("Entrez le prénom du médecin: ")
+        nom_medecin = input("Entrez le nom du médecin: ")
+
+        self.gestion_medecin.ajouter_medecin(prenom_medecin, nom_medecin)
+
+    def menu_supprimer_medecin(self):
+        id_medecin = input("Entrez l'ID du médecin à supprimer: ")
+
+        self.gestion_medecin.supprimer_medecin(id_medecin)
+
+    def menu_afficher_planning(self):
+        id_medecin = input("Entrez l'ID du médecin pour afficher le planning: ")
+        self.gestion_medecin.afficher_planning_semaine(id_medecin)
 
     def menu_ajouter_rdv(self):
         nom_patient = input("Entrez le nom du patient: ")
@@ -76,4 +139,3 @@ class InterfaceUtilisateur:
         heure_rdv = input("Entrez l'heure du rendez-vous à supprimer (format HH:MM): ")
 
         self.gestion_rdv.supprimer_rendezvous(nom_patient, prenom_patient, date_rdv, heure_rdv)
-
